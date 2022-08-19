82d0c85d25a80951f948bd400de8e52134fbaf3a
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-08-19 19:10:19 +0530

    second question

--- index.cpp
+++ index.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
+// 1:- Write a Program to print "Hello, World!"
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"NAMASTE DUNIYA !"<<endl;
+//     return 0;
+// };
+
+// 2) Write a Program to Print Integer Number Entered by Use
+
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"NAMASTE DUNIYA !"<<endl;
-    return 0;
+    int n;
+    cin>>n;
+    cout<<n<<endl;
 };
