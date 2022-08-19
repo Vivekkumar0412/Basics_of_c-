79abf2bd3a041c241c568fa1866da7bdca58e8be
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-08-19 19:19:41 +0530

    fifth

--- index.cpp
+++ index.cpp
@@ -35,14 +35,29 @@
 // remainder of their division is computed.(Both divisor and
 // dividend should be integers.)
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int divisor,divedent;
+//     cout<<"ENTER THE DIVISOR"<<endl;
+//     cin>>divisor;
+//     cout<<"ENTER THE DIVIDENT"<<endl;
+//     cin>>divedent;
+//     cout<<divedent/divisor<<endl;
+//     return 0;
+// };
+
+// 5) Write a Program to Find Size of int, float, double and char
+// in your computer
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int divisor,divedent;
-    cout<<"ENTER THE DIVISOR"<<endl;
-    cin>>divisor;
-    cout<<"ENTER THE DIVIDENT"<<endl;
-    cin>>divedent;
-    cout<<divedent/divisor<<endl;
+    int n = 9;
+    float c = 3.4;
+    char b = 'b';
+    double z = 1802009.308083803;
+
+    cout<<sizeof(n)<<sizeof(z)<<sizeof(b)<<sizeof(c);
     return 0;
-};
+}
