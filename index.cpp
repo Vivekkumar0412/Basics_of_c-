0e8c2c9381a1e36d34a951480c7d21e84b303241
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-08-25 08:36:54 +0530

    print hollow rectangle

--- index.cpp
+++ index.cpp
@@ -685,17 +685,42 @@
 //     };
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         for (int j = 1; j <= 5; j++)
+//         {
+//             cout << " * ";
+//         };
+//         cout << endl;
+//     };
+//     return 0;
+// };
+
+
+// print hollow rectangle
+
+//  *  *  *  *  * 
+//  *           *
+//  *           *
+//  *           *
+//  *  *  *  *  *
+
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    for (int i = 1; i <= 10; i++)
-    {
-        for (int j = 1; j <= 5; j++)
-        {
-            cout << " * ";
+int main(){
+    for(int i = 1; i<=5; i++){
+        for(int j = 1; j<=5; j++){
+            if(i ==1 || i == 5 || j == 1 || j==5){
+                cout<<" * ";
+            }else{
+                cout<<"   ";
+            }
         };
-        cout << endl;
-    };
-    return 0;
+        cout<<endl;
+    }
 };
+
